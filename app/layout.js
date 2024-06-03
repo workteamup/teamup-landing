285bb0281b4f5b771380c55d77836bf42844eecd
thehwlz <ignacioramoshowell@gmail.com>
2024-06-04 01:12:15 +0200

    added smooth scrolling

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={syne.className}>{children}</body>
     </html>
   );
