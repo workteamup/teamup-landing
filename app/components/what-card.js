d1d0aaf7cb6899f2e61976aa82f29b378bf117aa
thehwlz <ignacioramoshowell@gmail.com>
2024-06-27 18:29:37 +0200

    quick fix

--- app/components/what-card.js
+++ app/components/what-card.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 
 const WhatCard = ({ title, text, img }) => {
   return (
-    <div className="flex flex-col pt-16 w-full bg-blue-50 rounded-2xl items-center border border-blue-100 cursor-pointer hover:shadow-xl hover:border-200 group">
+    <div className="flex flex-col flex-grow pt-16 w-full bg-blue-50 rounded-2xl items-center border border-blue-100 cursor-pointer hover:shadow-xl hover:border-200 group min-h-full">
       <h2 className="text-2xl font-semibold text-center mb-2 group-hover:text-blue-700">
         {title}
       </h2>
 
-      <p className="text-base text-slate-500 text-center mb-14 px-3">{text}</p>
+      <p className="text-base text-slate-500 text-center mb-14 px-3 grow">
+        {text}
+      </p>
       <div className="h-[280px] w-[75%] bg-[url('https://media.tacdn.com/media/attractions-splice-spp-674x446/0c/04/55/9b.jpg')] mx-12 rounded-t-2xl relative border-t-8 border-x-8 border-slate-50 bg-slate-50">
         <Image
           unoptimized
