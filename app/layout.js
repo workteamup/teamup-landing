bace435e1363ac831de71e10344ec1b960224057
Juan_Lou <mettheland@gmail.com>
2024-08-14 19:49:30 +0200

    added Clarity tracking and changed output

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,13 @@ export default function RootLayout({ children }) {
           strategy="beforeInteractive"
         ></Script>
         <link rel="icon" href="/favicon.ico" />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "nn9ccsv4wl");`}
+        </Script>
       </head>
       <body className={`${sora.className} antialiased`}>{children}</body>
     </html>
