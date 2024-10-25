bf280c5c962dbc5d51637838c5b076ed5ccd93ab
Juan_Lou <mettheland@gmail.com>
2024-10-25 12:04:30 +0200

    Added google analytics script and automated deployment

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -34,6 +34,17 @@ export default async function LocaleLayout({children, params: {locale}}) {
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "nn9ccsv4wl");`}
           </Script>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-25N577RN4Z"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-25N577RN4Z');`}
+          </Script>
           {children}
         </NextIntlClientProvider>
       </body>
