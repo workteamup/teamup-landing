9caa2c76f3cc0bc1a3220863807a1a7ba2ea07c1
Juan_Lou <mettheland@gmail.com>
2025-01-18 22:09:30 +0100

    Updated clarity and google tags

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,26 @@ export default function RootLayout({ children }) {
         />
         <Script id="clarity-script" strategy="afterInteractive">
           {`(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "nn9ccsv4wl");`}
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "pvs27seu8b");`}
         </Script>
+
+        {/* Google Ads Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16735538512"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16735538512');
+          `}
+        </Script>
+
         <TranslationProvider>{children}</TranslationProvider>
       </body>
     </html>
