bf64e3ce74c4911071edfd8220c2ac0dd430fa78
thehwlz <ignacioramoshowell@gmail.com>
2025-05-19 19:22:12 +0200

    Remove unused components and files to streamline the codebase
    - Deleted multiple components including Avatars, BenefitCard, Benefits, Button, CTA, Events, Features, Footer, Hero, InfoBar, KitDigital, Navbar, and others to reduce clutter and improve maintainability.
    - Cleaned up associated files and references to enhance overall project organization and performance.

--- app/components/tag.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-const Tag = ({ children }) => {
-  return (
-    <div className="inline-flex items-center justify-center box-border text-sm text-center bg-blue-50 text-blue-600 min-h-7 py-1 px-2 rounded font-medium">{`#${children}`}</div>
-  );
-};
-
-export default Tag;
--- app/en/pages/layout.js
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="bg-slate-50 font-sans">
-      <div>{children}</div>
-    </div>
-  );
-}
